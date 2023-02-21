e20474b17e6bfb8fc411a777d59088bd7d2e3468
Huong <huongnguyen.nth1709@gmail.com>
2023-02-21 20:10:07 +0200

    add needed assets

--- frontend-react/src/components/Navbar/Navbar.js
+++ frontend-react/src/components/Navbar/Navbar.js
@@ -3,7 +3,13 @@ import React from 'react';
 import './Navbar.scss';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </nav>
+  )
 };
 
 export default Navbar;
