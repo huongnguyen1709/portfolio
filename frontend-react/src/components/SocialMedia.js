0b365fc81f396374c05429122148b82b56906ebb
Huong <huongnguyen.nth1709@gmail.com>
2023-02-25 21:27:21 +0200

    implement SocialMedia component & NavigationDots component

---
+++ frontend-react/src/components/SocialMedia.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+      <div>
+        <BsLinkedin />
+      </div>
+
+      <div>
+        <BsGithub />
+      </div>
+    </div>
+  );
+};
+
+export default SocialMedia;
